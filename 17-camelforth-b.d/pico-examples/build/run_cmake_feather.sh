e28ccd7bf5a925acc3f01b3cd321d5dac3918901
wa1tnr <wa1tnr@users.noreply.github.com>
2021-03-14 14:11:18 +0000

    From Appendix D of the SDK documentation
    	modified:   run_cmake_feather.sh
    
    On branch dvlp-aa

--- 17-camelforth-b.d/pico-examples/build/run_cmake_feather.sh
+++ 17-camelforth-b.d/pico-examples/build/run_cmake_feather.sh
@@ -3,7 +3,10 @@
 
 # $ sh ./run_cmake_feather.sh
 
-cmake -D"PICO_BOARD=adafruit_feather_rp2040" ..
+# may do this on the command line or run this script
+# example only - may be applied to any board.
+
+ cmake -D"PICO_BOARD=adafruit_feather_rp2040" ..  # dont miss the dot dot
 
 # ls ../../pico-sdk/src/boards/include/boards/adafruit_feather_rp2040.h
 #END.
