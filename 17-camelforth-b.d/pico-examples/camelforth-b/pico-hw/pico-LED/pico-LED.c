387bad7db1a6529e3be91f2a7043e5bb1c915226
wa1tnr <wa1tnr@users.noreply.github.com>
2021-04-14 13:35:31 +0000

    Variable length pip - for different targets
    Some targets have an LED that needs more ON time,
    to see it very much at all.
    
    Old default was too weak of a blink on some boards.
    
    This patch is meant to address D13 brightness.
    
    	modified:   pico-LED.c
    	modified:   pico-LED.h
    
    On branch dvlp-aa-all-a-

--- 17-camelforth-b.d/pico-examples/camelforth-b/pico-hw/pico-LED/pico-LED.c
+++ 17-camelforth-b.d/pico-examples/camelforth-b/pico-hw/pico-LED/pico-LED.c
@@ -14,22 +14,25 @@ void _pico_LED_init(void) {
     gpio_set_dir(LED_PIN, GPIO_OUT);
 }
 
+/*
+void _sht_sleep(void) {
+    for (volatile int i = 4777; i>0; i--) { }
+}
+*/
+extern void _sht_sleep(void); // pico-LED.h
+
 int _pico_pip(void) {
-    const uint LED_PIN = LED_PIN_MASTER; // pico_LED.h
+    const uint LED_PIN = LED_PIN_MASTER;
     gpio_put(LED_PIN, 1);
-    sleep_ms(11);
+    _sht_sleep();
     gpio_put(LED_PIN, 0);
     sleep_ms(11);
 }
 
 int _pico_LED(void) {
-    const uint LED_PIN = LED_PIN_MASTER; // pico_LED.h
-    // gpio_init(LED_PIN);
-    // gpio_set_dir(LED_PIN, GPIO_OUT);
-    // while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
-    // }
+    const uint LED_PIN = LED_PIN_MASTER;
+    gpio_put(LED_PIN, 1);
+    sleep_ms(2);
+    gpio_put(LED_PIN, 0);
+    sleep_ms(500);
 }
--- 17-camelforth-b.d/pico-examples/camelforth-b/pico-hw/pico-LED/pico-LED.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/pico-hw/pico-LED/pico-LED.h
@@ -9,18 +9,30 @@
 #ifdef _BOARDS_PICO_H
     #ifndef LED_PIN_MASTER
         #define LED_PIN_MASTER 25;
+        #define PIP_LENGTH 4777 // untested
+    #endif
+#endif
+
+// itsybitsy
+// https://github.com/adafruit/circuitpython/blob/main/ports/raspberrypi/boards/adafruit_itsybitsy_rp2040/pins.c#L32
+#ifdef ADAFRUIT_ITSYBITSY_RP2040
+    #ifndef LED_PIN_MASTER
+        #define LED_PIN_MASTER 11;
+        #define PIP_LENGTH 4777 // probably okay
     #endif
 #endif
 
 #ifdef ADAFRUIT_FEATHER_RP2040
     #ifndef LED_PIN_MASTER
         #define LED_PIN_MASTER 13;
+        #define PIP_LENGTH 74777
     #endif
 #endif
 
 #ifdef PIMORONI_TINY2040
     #ifndef LED_PIN_MASTER
         #define LED_PIN_MASTER 13; // NO RESEARCH - validate me
+        #define PIP_LENGTH 74777 // untested - no target locally available to try this
     #endif
 #endif
 
@@ -28,5 +40,10 @@
     #warning -7777 // pico-LED.h  found no researched definition for the onboard LED pin
     #warning -7778 // pico-LED.h  LED_PIN_MASTER assigned to GPIO13, on a guess!
     #define LED_PIN_MASTER 13 // assumes 'standard' D13 assignment per Arduino IDE sensibilities
+    #define PIP_LENGTH 74777 // generic
     // -7777 // throw compile error
 #endif
+
+void _sht_sleep(void) {
+    for (volatile int i = PIP_LENGTH ; i>0; i--) { }
+}
