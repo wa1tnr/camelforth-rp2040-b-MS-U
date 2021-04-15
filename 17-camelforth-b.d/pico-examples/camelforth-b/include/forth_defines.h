38c9467358eb5cf5807b9b5209f049e0acab2601
wa1tnr <wa1tnr@users.noreply.github.com>
2021-04-15 17:52:06 +0000

    Establish text of various STAMP defines
    	new file:   forth_defines.h
    
    On branch dvlp-aa--headers-

---
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
@@ -0,0 +1,8 @@
+
+// #warning seen forth_defines.h
+
+#define RECENT_STAMP      "Thu Apr 15 17:13:15 UTC 2021"
+#define COMMIT_TIME_STAMP "Thu Apr 15 16:49:01 UTC 2021"
+#define BRANCH_STAMP      "dvlp-aa--headers-               0.1.7-pre-alpha"
+#define COMMIT_STAMP      "e1a381a" // seven characters
+#define FEATURE_STAMP     "+headers +fl_sizing +alltargets      "
