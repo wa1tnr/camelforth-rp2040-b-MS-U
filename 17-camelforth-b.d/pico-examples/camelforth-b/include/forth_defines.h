926393946a1c5ff9ef85a1b79ea484d0342af221
wa1tnr <wa1tnr@users.noreply.github.com>
2021-04-15 20:00:27 +0000

    forth defines timestamps and such

--- 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
@@ -1,8 +1,8 @@
 
 // #warning seen forth_defines.h
 
-#define RECENT_STAMP      "Thu Apr 15 17:13:15 UTC 2021"
-#define COMMIT_TIME_STAMP "Thu Apr 15 16:49:01 UTC 2021"
-#define BRANCH_STAMP      "dvlp-aa--headers-               0.1.7-pre-alpha"
-#define COMMIT_STAMP      "e1a381a" // seven characters
+#define RECENT_STAMP      "Thu Apr 15 19:55:45 UTC 2021"
+#define COMMIT_TIME_STAMP "Thu Apr 15 19:53:02 UTC 2021"
+#define BRANCH_STAMP      "dvlp-aa                         0.1.7-pre-alpha"
+#define COMMIT_STAMP      "9d6e632" // seven characters
 #define FEATURE_STAMP     "+headers +fl_sizing +alltargets      "
