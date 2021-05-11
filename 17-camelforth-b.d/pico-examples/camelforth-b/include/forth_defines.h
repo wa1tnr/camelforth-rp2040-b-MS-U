47d15edc7f7f440e264a7d46373cc164a11ffed2
wa1tnr <wa1tnr@users.noreply.github.com>
2021-05-11 19:04:26 +0000

    Comprehensive update WS2812 all targets
    Some targets don't have an inbuilt WS2812.
    
    They were given arbitrary pin assignments.
    Power pins were not allocated to them.
    
    	modified:   forth_defines.h
    
    On branch dvlp-aa

--- 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
@@ -1,10 +1,8 @@
 
 // #warning seen forth_defines.h
 
-#define RECENT_STAMP      "Tue May 11 16:51:36 UTC 2021"
-#define COMMIT_TIME_STAMP "Tue May 11 16:48:01 UTC 2021"
-// #define BRANCH_STAMP   "dvlp-aa                         0.1.7-pre-alpha"
-#define BRANCH_STAMP      "dvlp-aa-ws2812-a-               0.1.7-pre-alpha"
-#define COMMIT_STAMP      "a65e1d8" // seven characters
-// #define FEATURE_STAMP  "+headers +fl_sizing +alltargets      "
+#define RECENT_STAMP      "Tue May 11 19:00:48 UTC 2021"
+#define COMMIT_TIME_STAMP "Tue May 11 18:58:00 UTC 2021"
+#define BRANCH_STAMP      "dvlp-aa                         0.1.7-pre-alpha"
+#define COMMIT_STAMP      "3725912" // seven characters
 #define FEATURE_STAMP     "+neopix +headers +fl_sizing          "
