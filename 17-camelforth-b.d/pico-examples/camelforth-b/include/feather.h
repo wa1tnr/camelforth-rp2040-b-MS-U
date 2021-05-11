af3ecd28b91bcd673a7dfa4114816346b2a7d6ee
wa1tnr <wa1tnr@users.noreply.github.com>
2021-05-11 18:55:51 +0000

    removed semicolon added NPX defines

--- 17-camelforth-b.d/pico-examples/camelforth-b/include/feather.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/feather.h
@@ -5,7 +5,9 @@
 #ifdef ADAFRUIT_FEATHER_RP2040
 
     #ifndef LED_PIN_MASTER
-        #define LED_PIN_MASTER 13;
+        #define LED_PIN_MASTER 13
+        #define NPX_PIN 16
+        #define NPX_PWR_PIN -1
         #define PIP_LENGTH 74777
         #define KB_PIP_LN  2444
         #define BLINK_MS_CF 60
