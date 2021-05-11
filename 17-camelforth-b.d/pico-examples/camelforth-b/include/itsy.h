f66d2301f7524b297fc05b67189716593719d559
wa1tnr <wa1tnr@users.noreply.github.com>
2021-05-11 18:56:23 +0000

    added NPX defines

--- 17-camelforth-b.d/pico-examples/camelforth-b/include/itsy.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/itsy.h
@@ -4,9 +4,10 @@
 // https://github.com/adafruit/circuitpython/blob/main/ports/raspberrypi/boards/adafruit_itsybitsy_rp2040/pins.c#L32
 // boot2_w25q080
 #ifdef ADAFRUIT_ITSYBITSY_RP2040
-
     #ifndef LED_PIN_MASTER
-        #define LED_PIN_MASTER 11;
+        #define LED_PIN_MASTER 11
+        #define NPX_PIN 17
+        #define NPX_PWR_PIN 16
         #define PIP_LENGTH 19777 // probably okay
         #define KB_PIP_LN  2444
         #define BLINK_MS_CF 60
@@ -17,6 +18,4 @@
         #define FLASH_MASK  0x3FF000
         #define CF_PICO_PLATFORM "Adafruit ItsyBitsy RP2040"
     #endif
-
 #endif
-
