3725912585ff91a0e47dd3db5a6f406e1f5b83bb
wa1tnr <wa1tnr@users.noreply.github.com>
2021-05-11 18:58:00 +0000

    arbitrary pin assigned for external WS2812 duty

--- 17-camelforth-b.d/pico-examples/camelforth-b/include/tiny2040.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/tiny2040.h
@@ -4,8 +4,12 @@
 
 #ifdef PIMORONI_TINY2040
 
+#warning UNTESTED AND UNRESEARCHED USE CAUTION
+
     #ifndef LED_PIN_MASTER
         #define LED_PIN_MASTER 13; // NO RESEARCH - validate me
+        #define NPX_PIN 19 // arbitrary external WS2812
+        #define NPX_PWR_PIN -1
         #define PIP_LENGTH 74777 // untested - no target locally available to try this
         #define KB_PIP_LN  2444
         #define BLINK_MS_CF 60
