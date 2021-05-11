0284126d39fed57671943b7e8f182e34568ec46f
wa1tnr <wa1tnr@users.noreply.github.com>
2021-05-11 21:42:40 +0000

    Latest timestamps - a few commits behind the present
    	modified:   forth_defines.h
    
    On branch dvlp-aa

--- 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
@@ -2,7 +2,7 @@
 // #warning seen forth_defines.h
 
 #define RECENT_STAMP      "Tue May 11 19:00:48 UTC 2021"
-#define COMMIT_TIME_STAMP "Tue May 11 18:58:00 UTC 2021"
-#define BRANCH_STAMP      "dvlp-aa                         0.1.7-pre-alpha"
-#define COMMIT_STAMP      "3725912" // seven characters
-#define FEATURE_STAMP     "+neopix +headers +fl_sizing          "
+#define COMMIT_TIME_STAMP "Tue May 11 19:46:21 UTC 2021"
+#define BRANCH_STAMP      "dvlp-aa                         0.1.8-pre-alpha"
+#define COMMIT_STAMP      "4aa9d5e" // seven characters
+#define FEATURE_STAMP     "+clkdiv_hack +neopix +headers        "
