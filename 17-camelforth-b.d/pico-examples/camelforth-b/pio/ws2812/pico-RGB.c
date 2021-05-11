56e672123517431ccc1ade6ba4bad850fbf16a41
wa1tnr <wa1tnr@users.noreply.github.com>
2021-05-11 18:54:21 +0000

    generalized to any target board

--- 17-camelforth-b.d/pico-examples/camelforth-b/pio/ws2812/pico-RGB.c
+++ 17-camelforth-b.d/pico-examples/camelforth-b/pio/ws2812/pico-RGB.c
@@ -1,17 +1,24 @@
+#include <stdio.h> // puts
 #include "pico/stdlib.h"
+#include "pico-hardware-camelforth.h" // NPX_PWR_PIN defined
 
 // NEO_PWR is 11, QTPY RP2040
 // NEOPIX is 12, QTPY RP2040
-#define RGB_PIN_MASTER 11
+// #define RGB_PWR_PIN NPX_PWR_PIN
 // #define RGB_PIN_MASTER 16 // rename as NEO_PWR
 // NEOPIX on 17 ItsyRP2040
 
 void _pico_RGB_init(void) {
-    const uint RGB_PIN = RGB_PIN_MASTER;
-    gpio_init(RGB_PIN);
+    if (NPX_PWR_PIN == -1) {
+        // puts("ck77p");
+        // puts("Skipping power pin init entirely. Use no GPIO pin to power the NEOPIX.");
+        return;
+    }
+    const uint RGB_PWR_PIN = NPX_PWR_PIN;
+    gpio_init(RGB_PWR_PIN);
     // is this enough to operate push-pull:
-    gpio_set_dir(RGB_PIN, GPIO_OUT);
-    gpio_put(RGB_PIN, 1); // feather: raise D11 no harm done,
-                          // feather: but no help, either
-                          // feather: as its ws2812b has power 3.3 full time
+    gpio_set_dir(RGB_PWR_PIN, GPIO_OUT);
+    gpio_put(RGB_PWR_PIN, 1); // feather: raise D11 no harm done,
+                              // feather: but no help, either
+                              // feather: as its ws2812b has power 3.3 full time
 }
