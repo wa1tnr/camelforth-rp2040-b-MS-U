222fa49c0acc4aa94473a19c75cb8cecdef52184
wa1tnr <wa1tnr@users.noreply.github.com>
2021-05-11 18:56:48 +0000

    NPX pin defines

--- 17-camelforth-b.d/pico-examples/camelforth-b/include/qtpy.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/qtpy.h
@@ -1,12 +1,13 @@
 // #include "pico/stdlib.h"
 
 // #warning was called in qtpy.h
-#warning was called in qtpy.h
 
 #ifdef ADAFRUIT_QTPY_RP2040
 
     #ifndef LED_PIN_MASTER
-        #define LED_PIN_MASTER 26;
+        #define LED_PIN_MASTER 26
+        #define NPX_PIN 12
+        #define NPX_PWR_PIN 11
         #define PIP_LENGTH 74777
         #define KB_PIP_LN  2444
         #define BLINK_MS_CF 60
