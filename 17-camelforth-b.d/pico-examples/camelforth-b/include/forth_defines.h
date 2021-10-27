2c76db147ed592893b9c2fe687b9bfb162c44c41
wa1tnr <wa1tnr@users.noreply.github.com>
2021-10-27 13:23:00 +0000

    Defines used in prebuilt UF2
    	modified:   forth_defines.h
    
    On branch dvlp-aa

--- 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
@@ -1,8 +1,8 @@
 
 // #warning seen forth_defines.h
 
-#define RECENT_STAMP      "Tue May 11 22:08:34 UTC 2021"
-#define COMMIT_TIME_STAMP "Tue May 11 21:42:40 UTC 2021"
+#define RECENT_STAMP      "Wed Oct 27 12:50:11 UTC 2021"
+#define COMMIT_TIME_STAMP "Wed May 12 21:18:08 UTC 2021"
 #define BRANCH_STAMP      "dvlp-aa                         0.1.8-pre-alpha"
-#define COMMIT_STAMP      "0284126" // seven characters
+#define COMMIT_STAMP      "1f700c1" // seven characters
 #define FEATURE_STAMP     "+clkdiv_hack +neopix +headers        "
