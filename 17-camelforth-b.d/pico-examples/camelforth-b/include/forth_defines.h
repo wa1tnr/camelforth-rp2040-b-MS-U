07d51fbeaa05ce151f78e896d77ec0c1c1c3280d
wa1tnr <wa1tnr@users.noreply.github.com>
2023-03-30 23:00:38 +0000

    Up to date w ASM w00t
      well except for the files themselves ;)
      Not yet committed.
    
    	modified:   forth_defines.h
    
    On branch dvlp-asm-aa

--- 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
@@ -1,7 +1,8 @@
-#warning seen 30 Mar 2023 forth_defines.h
+// #warning seen 30 Mar 2023 forth_defines.h
 
-#define RECENT_STAMP      "Thu 30 Mar 22:22:12 UTC 2023"
-#define COMMIT_TIME_STAMP "Mon Nov 14 17:49:55 UTC 2022"
-#define BRANCH_STAMP      "dvlp-aa                         0.1.8-pre-alpha"
-#define COMMIT_STAMP      "adf3333" // seven characters
-#define FEATURE_STAMP     "+ASM BOX 30 Mar +no_flash +clkdiv_ha "
+#define RECENT_STAMP      "Thu 30 Mar 22:51:41 UTC 2023"
+#define COMMIT_TIME_STAMP "Thu Mar 30 22:46:07 UTC 2023"
+#define BRANCH_STAMP      "dvlp-asm-aa                     0.1.8-pre-alpha"
+#define COMMIT_STAMP      "14e421d" // seven characters
+#define FEATURE_STAMP     "+ASM BOX +github +no_flash +clkdiv_h "
+
