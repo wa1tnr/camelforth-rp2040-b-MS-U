14e421de44f32b448f5c4ca2f19cc043ed02a143
wa1tnr <wa1tnr@users.noreply.github.com>
2023-03-30 22:46:07 +0000

    defines for 30 Mar 2023

--- 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
@@ -1,8 +1,7 @@
+#warning seen 30 Mar 2023 forth_defines.h
 
-// #warning seen forth_defines.h
-
-#define RECENT_STAMP      "Wed Oct 27 12:50:11 UTC 2021"
-#define COMMIT_TIME_STAMP "Wed May 12 21:18:08 UTC 2021"
+#define RECENT_STAMP      "Thu 30 Mar 22:22:12 UTC 2023"
+#define COMMIT_TIME_STAMP "Mon Nov 14 17:49:55 UTC 2022"
 #define BRANCH_STAMP      "dvlp-aa                         0.1.8-pre-alpha"
-#define COMMIT_STAMP      "1f700c1" // seven characters
-#define FEATURE_STAMP     "+clkdiv_hack +neopix +headers        "
+#define COMMIT_STAMP      "adf3333" // seven characters
+#define FEATURE_STAMP     "+ASM BOX 30 Mar +no_flash +clkdiv_ha "
