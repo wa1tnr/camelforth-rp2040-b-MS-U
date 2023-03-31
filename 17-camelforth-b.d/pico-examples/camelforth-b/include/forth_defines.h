c6cc55e06e532433e1bcd74ba65d4d0ae6003ce2
wa1tnr <wa1tnr@users.noreply.github.com>
2023-03-31 00:14:52 +0000

    blind commit

--- 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
@@ -1,8 +1,8 @@
 // #warning seen 30 Mar 2023 forth_defines.h
 
-#define RECENT_STAMP      "Thu 30 Mar 22:51:41 UTC 2023"
-#define COMMIT_TIME_STAMP "Thu Mar 30 22:46:07 UTC 2023"
-#define BRANCH_STAMP      "dvlp-asm-aa                     0.1.8-pre-alpha"
-#define COMMIT_STAMP      "14e421d" // seven characters
+// RECENT_STAMP is for after a commit when there is a change
+#define RECENT_STAMP      "Fri Mar 31 00:10:34 UTC 2023"
+#define COMMIT_TIME_STAMP "Fri Mar 31 00:10:34 UTC 2023"
+#define BRANCH_STAMP      "dvlp-asm-aa   later alligator   0.1.8-pre-alpha"
+#define COMMIT_STAMP      "b70b8b7" // seven characters
 #define FEATURE_STAMP     "+ASM BOX +github +no_flash +clkdiv_h "
-
