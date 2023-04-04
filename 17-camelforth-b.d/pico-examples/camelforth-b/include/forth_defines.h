bf2500fe8324ed0ba936d3a2f32a1b8daf856994
wa1tnr <wa1tnr@users.noreply.github.com>
2023-04-04 01:36:40 +0000

    parameter passing from C to asm
      Program has a define for blink count
      in the .c source file asmword.c
      Expecting to use TOS there fairly soon.
    
    	modified:   forth_defines.h
    
    On branch asm-dvlp-aa

--- 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
@@ -1,8 +1,8 @@
-// #warning seen 30 Mar 2023 forth_defines.h
+// #warning seen 04 Apr 2023 forth_defines.h
 
 // RECENT_STAMP is for after a commit when there is a change
-#define RECENT_STAMP      "Fri Mar 31 00:10:34 UTC 2023"
-#define COMMIT_TIME_STAMP "Fri Mar 31 00:10:34 UTC 2023"
-#define BRANCH_STAMP      "dvlp-asm-aa   later alligator   0.1.8-pre-alpha"
-#define COMMIT_STAMP      "b70b8b7" // seven characters
-#define FEATURE_STAMP     "+ASM BOX +github +no_flash +clkdiv_h "
+#define RECENT_STAMP      "Tue Apr  4 01:24:24 UTC 2023"
+#define COMMIT_TIME_STAMP "Tue Apr  4 00:54:32 UTC 2023"
+#define BRANCH_STAMP      "asm-dvlp-aa  later edits f7e4   0.1.9-pre-alpha"
+#define COMMIT_STAMP      "f8680d9" // seven characters
+#define FEATURE_STAMP     "+asm_params +ASM BOX +github +no_fla "
