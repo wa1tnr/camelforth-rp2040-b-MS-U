6c44db3b5a7f96f2e2e62c70756c2850b3e74f4e
wa1tnr <wa1tnr@users.noreply.github.com>
2023-04-04 14:12:16 +0000

    last of bugfixes for now -Wall good
       note: one forth.h error is in upstream macro
       and was fixed on the side - not committed yet.
    
    	modified:   forth_defines.h
    
    On branch asm-dvlp-bugfix

--- 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
@@ -1,8 +1,11 @@
 // #warning seen 04 Apr 2023 forth_defines.h
 
 // RECENT_STAMP is for after a commit when there is a change
-#define RECENT_STAMP      "Tue Apr  4 01:24:24 UTC 2023"
+#define RECENT_STAMP      "Tue Apr  4 13:35:03 UTC 2023"
 #define COMMIT_TIME_STAMP "Tue Apr  4 00:54:32 UTC 2023"
-#define BRANCH_STAMP      "asm-dvlp-aa  later edits f7e4   0.1.9-pre-alpha"
+#define BRANCH_STAMP      "asm-dvlp-bugfix  r.00a- -Wall   0.1.9-pre-alpha"
 #define COMMIT_STAMP      "f8680d9" // seven characters
 #define FEATURE_STAMP     "+asm_params +ASM BOX +github +no_fla "
+#define WOKWI_PRESENT -1
+
+// #define BRANCH_STAMP      "asm-dvlp-aa  later edits f7e4   0.1.9-pre-alpha"
