05d635cf01677bf69d0bea2340fa41659c1d5bf5
wa1tnr <wa1tnr@users.noreply.github.com>
2023-04-04 00:46:30 +0000

    pass parameters to assembler - aa
    	modified:   asmword.c
    
    On branch asm-dvlp-aa

--- 17-camelforth-b.d/pico-examples/camelforth-b/asm/asmword.c
+++ 17-camelforth-b.d/pico-examples/camelforth-b/asm/asmword.c
@@ -4,6 +4,8 @@
 
 // @ https://wokwi.com/projects/360645588605043713
 
+#define PBLINKS 5 // how many blinks?
+
 #include "hardware/gpio.h"
 
 #ifdef __cplusplus
@@ -17,7 +19,13 @@ void experiment_a_asm();
 #endif
 
 void asmword(void) {
-	return experiment_a_asm();
+    volatile static int c;
+    c = (PBLINKS * 2) + 2;
+    char *ram;
+    ram = (char *) c;
+    unsigned int *cptr = &ram;
+    // passes both the value and the address to assembler routine:
+    return experiment_a_asm(c, cptr);
 }
 
 // END.
