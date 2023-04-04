99f19c2b3f5ce31e92438b57b31e3bc941011186
wa1tnr <wa1tnr@users.noreply.github.com>
2023-04-04 17:36:43 +0000

    strip ^M line endings

--- 17-camelforth-b.d/pico-examples/camelforth-b/forth/forth.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/forth/forth.h
@@ -5,23 +5,23 @@
  *  Header file for forth.c
  ******
  * LICENSE TERMS
- *  CamelForth in C 
+ *  CamelForth in C
  *  copyright (c) 2016,2017 Bradford J. Rodriguez.
- *  
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *  
- *  Commercial inquiries should be directed to the author at 
+ *
+ *  Commercial inquiries should be directed to the author at
  *  115 First St., #105, Collingwood, Ontario L9Y 4W3 Canada
  *  or via email to bj@camelforth.com
  */
@@ -39,7 +39,7 @@
 #define RP2040_PICO               /* for use with Raspberry Pi Pico RP2040 based target */
 #define USB_IFACE                 /* only some implementations */
 
-/* 
+/*
  * CONFIGURATION PARAMETERS
  */
 
@@ -74,7 +74,7 @@ struct Header {
     char nfa[];             /* inline name string */
 };
 #endif
- 
+
 #define HEADER(name,prev,flags,namestring) struct Header H##name =\
     { (char *)H##prev.nfa, T##name, flags, namestring }
 #define IMMEDIATE 1         /* immediate bit in flags */
